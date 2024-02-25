d36cacda8a0e2054e8c97ac62045da7a365d8dde
augusstt06 <augusstt06@gmail.com>
2024-02-25 19:38:10 +0900

    fix : mount effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { useState } from 'react'
 
 import { Outfit } from 'next/font/google'
 
-import Korea from './map/Korea'
+import Korea from './_map/Korea'
 import './globals.css'
 
 const outfit = Outfit({
@@ -17,20 +17,30 @@ export default function Home() {
     setShowMap(true)
   }
   return (
-    <div id="main" className="h-screen" onClick={handleOverlayClick}>
-      <div className="flex flex-col absolute lg:top-1/3 lg:left-40 lg:transform-none lg:text-left top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center space-y-4">
-        <h1 className={`text-white text-4xl ${outfit.className}`}>
-          Tour in Korea
-        </h1>
-        <p className={`text-white ${outfit.className}`}>
-          Explore various tourist attractions located in Korea
-        </p>
-      </div>
-      {showMap && (
-        <main className="flex justify-center items-center h-screen">
+    <main id="main" className="h-screen">
+      {showMap ? (
+        <section
+          id="map"
+          className="flex justify-center items-center h-screen backdrop-blur-sm	"
+        >
           <Korea />
-        </main>
+        </section>
+      ) : (
+        <section className="flex flex-col absolute lg:top-1/3 lg:left-40 lg:transform-none lg:text-left top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center space-y-4">
+          <h1 className={`text-white text-4xl ${outfit.className}`}>
+            Tour in Korea
+          </h1>
+          <p className={`text-white ${outfit.className}`}>
+            Explore various tourist attractions located in Korea
+          </p>
+          <button
+            className={`transition ease-in-out duration-1000 p-2 bg-indigo-800 w-1/3 rounded-xl text-white bg-opacity-20 backdrop-blur-sm hover:scale-110 hover:bg-opacity-40 ${outfit.className}`}
+            onClick={handleOverlayClick}
+          >
+            Let&apos;s Explore!
+          </button>
+        </section>
       )}
-    </div>
+    </main>
   )
 }
