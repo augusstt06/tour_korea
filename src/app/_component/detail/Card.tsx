3e848995fe7916eb3a6dc56e2d810d1a40e8cdd6
augusstt06 <augusstt06@gmail.com>
2024-02-27 15:16:42 +0900

    add : apply loading component

--- src/app/_component/detail/Card.tsx
+++ src/app/_component/detail/Card.tsx
@@ -1,13 +1,23 @@
+import Loading from '../loading/Loading'
+
 import { type DetailProps } from '@/app/_type'
 
 export default function Detail(props: DetailProps) {
   const { place } = props
+  const renderDetail = () => {
+    if (place === undefined) return <Loading />
+    return (
+      <>
+        <h1>{place.title}</h1>
+        <p>{place.addr1}</p>
+        <p>{place.addr2}</p>
+      </>
+    )
+  }
   return (
     <details>
       <summary>추가 정보</summary>
-      <h1>{place.title}</h1>
-      <p>{place.addr1}</p>
-      <p>{place.addr2}</p>
+      {renderDetail()}
     </details>
   )
 }
